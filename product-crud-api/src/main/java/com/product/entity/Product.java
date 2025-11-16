9f2d655c9362051ed16b03550453efe69bdb71ea
Sanjeev Kumar <109436065+sanjeev51197@users.noreply.github.com>
2025-11-16 21:13:33 +0530

    Update Product.java

--- product-crud-api/src/main/java/com/product/entity/Product.java
+++ product-crud-api/src/main/java/com/product/entity/Product.java
@@ -17,7 +17,7 @@ public class Product {
 	private double price;
         private Date expiryDate;
         private boolean isAvailable;
-	
+	    private String productCode;
 	public long getPid() {
 		return pid;
 	}
